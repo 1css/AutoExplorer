6679f865ac256944acd4c27e8f9d70f74c0ebb44
position-temp <harshith.c.s.2001@gmail.com>
2025-02-21 09:53:42 +0530

    update conf

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   env: {
     VITE_NODE_ENV: "development",
   },
   build: {
-    outDir: "build",
+    outDir: "dist", // Ensure this matches the directory you expect
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
   },
 });
